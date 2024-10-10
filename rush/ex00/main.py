4d269c561387c98bd7b30d81c79c0701318efd44
Gongtozazath <164620829+Gongtozazath@users.noreply.github.com>
2024-10-10 20:01:36 +0700

    Update main.py

--- rush/ex00/main.py
+++ rush/ex00/main.py
@@ -1,16 +1,16 @@
 # main.py
 
-from checkmate import checkmate
+from checkmate import checkmate #นำเข้าฟังก์ชัน checkmate จาก checkmate.py
 
-def main():
+
+def main(): #ใช้ฟังก์ชัน main เพื่อกำหนดกระดานหมาก R=Rook Q=Queen K=King P=Pawn
     board = """\
-R...
-.Q..
+...Q
+....
 ..K.
 ....\
 """
-    checkmate(board)
+    checkmate(board) #ใช้ฟังก์ชัน checkmate เพื่อเช็คตำแหน่งหมาก
 
-if __name__ == "__main__":
+if __name__ == "__main__": #ใช้นำข้อมูลจาก checkmate.py มาใช้ใน main.py เพิ่อแสดงผลลัพธ์
     main()
-
