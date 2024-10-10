ffdeff17648d521c02a417ffcba4e752688ab405
Gongtozazath <164620829+Gongtozazath@users.noreply.github.com>
2024-10-10 11:25:42 +0700

    Update main.py

--- rush/ex00/main.py
+++ rush/ex00/main.py
@@ -1,92 +1,189 @@
+import itertools
+WHITE = "white"
+BLACK = "black"
 
-# ฟังก์ชันหลักสำหรับตรวจสอบว่าราชาอยู่ในสถานการณ์รุกหรือไม่
-def is_king_in_check(board):
-    king_position = find_king(board)
-    
-    if king_position is None:
-        print("Error: King not found")
-        return
-    
-    # ตรวจสอบตัวหมากแต่ละตัวบนกระดาน
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            piece = board[row][col]
-            
-            if piece == 'P':  # เบี้ย
-                if is_pawn_attacking(row, col, king_position, board):
-                    print("Success")
-                    return
-            elif piece == 'B':  # บิชอป
-                if is_bishop_attacking(row, col, king_position, board):
-                    print("Success")
-                    return
-            elif piece == 'R':  # เรือ
-                if is_rook_attacking(row, col, king_position, board):
-                    print("Success")
-                    return
-            elif piece == 'Q':  # ราชินี
-                if is_queen_attacking(row, col, king_position, board):
-                    print("Success")
-                    return
-    
-    # ถ้าไม่มีตัวหมากไหนโจมตีราชาได้
-    print("Fail")
+class Game:
+    def __init__(self):
+        self.playersturn = BLACK
+        self.message = "this is where prompts will go"
+        self.gameboard = {}
+        self.placePieces()
+        print("chess program. enter moves in algebraic notation separated by space")
+        self.main()
 
-# ฟังก์ชันค้นหาตำแหน่งของราชาบนกระดาน
-def find_king(board):
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            if board[row][col] == 'K':
-                return (row, col)
-    return None  # กรณีที่ไม่มีราชา (ไม่ควรเกิดขึ้น)
+    def placePieces(self):
+        # วางตำแหน่งเบี้ย
+        for i in range(0,8):
+            self.gameboard[(i,1)] = Pawn(WHITE,uniDict[WHITE][Pawn],1)
+            self.gameboard[(i,6)] = Pawn(BLACK,uniDict[BLACK][Pawn],-1)
+        
+        # วางตำแหน่งหมากอื่น ๆ ยกเว้นม้า
+        placers = [Rook,Bishop,Queen,King,Bishop,Rook]
+        
+        for i in range(0,8):
+            self.gameboard[(i,0)] = placers[i](WHITE,uniDict[WHITE][placers[i]])
+            self.gameboard[((7-i),7)] = placers[i](BLACK,uniDict[BLACK][placers[i]])
+        placers.reverse()
 
-# ฟังก์ชันตรวจสอบการโจมตีจากเบี้ย
-def is_pawn_attacking(pawn_row, pawn_col, king_position, board):
-    king_row, king_col = king_position
-    # ตรวจสอบว่าราชาอยู่ในทิศทางที่เบี้ยโจมตี (ทแยงมุมหนึ่งช่องไปข้างหน้า)
-    # สมมติว่าเป็นเบี้ยขาวที่เดินขึ้น
-    return (pawn_row - 1, pawn_col - 1) == king_position or (pawn_row - 1, pawn_col + 1) == king_position
+    def main(self):
+        while True:
+            self.printBoard()
+            print(self.message)
+            self.message = ""
+            startpos,endpos = self.parseInput()
+            try:
+                target = self.gameboard[startpos]
+            except:
+                self.message = "could not find piece; index probably out of range"
+                target = None
+                
+            if target:
+                print("found "+str(target))
+                if target.Color != self.playersturn:
+                    self.message = "you aren't allowed to move that piece this turn"
+                    continue
+                if target.isValid(startpos,endpos,target.Color,self.gameboard):
+                    self.message = "that is a valid move"
+                    self.gameboard[endpos] = self.gameboard[startpos]
+                    del self.gameboard[startpos]
+                    self.isCheck()
+                    if self.playersturn == BLACK:
+                        self.playersturn = WHITE
+                    else: 
+                        self.playersturn = BLACK
+                else: 
+                    self.message = "invalid move" + str(target.availableMoves(startpos[0],startpos[1],self.gameboard))
+                    print(target.availableMoves(startpos[0],startpos[1],self.gameboard))
+            else: 
+                self.message = "there is no piece in that space"
 
-# ฟังก์ชันตรวจสอบการโจมตีจากบิชอป
-def is_bishop_attacking(bishop_row, bishop_col, king_position, board):
-    return check_diagonal(bishop_row, bishop_col, king_position, board)
-
-# ฟังก์ชันตรวจสอบการโจมตีจากเรือ
-def is_rook_attacking(rook_row, rook_col, king_position, board):
-    return check_straight(rook_row, rook_col, king_position, board)
-
-# ฟังก์ชันตรวจสอบการโจมตีจากราชินี (ทั้งทแยงและตรง)
-def is_queen_attacking(queen_row, queen_col, king_position, board):
-    return check_diagonal(queen_row, queen_col, king_position, board) or check_straight(queen_row, queen_col, king_position, board)
-
-# ฟังก์ชันตรวจสอบการโจมตีในแนวทแยง (สำหรับบิชอปและราชินี)
-def check_diagonal(start_row, start_col, king_position, board):
-    king_row, king_col = king_position
-    # ตรวจสอบทุกแนวทแยง (ซ้ายบน, ขวาบน, ซ้ายล่าง, ขวาล่าง)
-    directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
-    for direction in directions:
-        row, col = start_row, start_col
-        while 0 <= row < len(board) and 0 <= col < len(board):
-            row += direction[0]
-            col += direction[1]
-            if (row, col) == king_position:
+    def isCheck(self):
+        # หาตำแหน่งของกษัตริย์และตรวจสอบว่าถูกคุกคามหรือไม่
+        kingDict = {}
+        pieceDict = {BLACK : [], WHITE : []}
+        for position, piece in self.gameboard.items():
+            if type(piece) == King:
+                kingDict[piece.Color] = position
+            pieceDict[piece.Color].append((piece, position))
+        # ตรวจสอบว่ากษัตริย์ฝ่ายขาวหรือดำอยู่ในเช็คหรือไม่
+        if self.canSeeKing(kingDict[WHITE], pieceDict[BLACK]):
+            self.message = "White player is in check"
+        if self.canSeeKing(kingDict[BLACK], pieceDict[WHITE]):
+            self.message = "Black player is in check"
+        
+    def canSeeKing(self, kingpos, piecelist):
+        # ตรวจสอบว่าชิ้นหมากจากรายการสามารถโจมตีกษัตริย์ได้หรือไม่
+        for piece, position in piecelist:
+            if piece.isValid(position, kingpos, piece.Color, self.gameboard):
                 return True
-            if not (0 <= row < len(board)) or not (0 <= col < len(board[0])) or board[row][col] != '.':  # เจอหมากขวางหรือออกนอกกระดาน
-                break
-    return False
+        return False
+                
+    def parseInput(self):
+        try:
+            a, b = input().split()
+            a = ((ord(a[0])-97), int(a[1])-1)
+            b = (ord(b[0])-97, int(b[1])-1)
+            return (a, b)
+        except:
+            print("error decoding input. please try again")
+            return((-1, -1), (-1, -1))
 
-# ฟังก์ชันตรวจสอบการโจมตีในแนวตรง (สำหรับเรือและราชินี)
-def check_straight(start_row, start_col, king_position, board):
-    king_row, king_col = king_position
-    # ตรวจสอบแนวตั้งและแนวนอน
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    for direction in directions:
-        row, col = start_row, start_col
-        while 0 <= row < len(board) and 0 <= col < len(board):
-            row += direction[0]
-            col += direction[1]
-            if (row, col) == king_position:
-                return True
-            if not (0 <= row < len(board)) or not (0 <= col < len(board[0])) or board[row][col] != '.':  # เจอหมากขวางหรือออกนอกกระดาน
-                break
-    return False
+    def printBoard(self):
+        print("  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |")
+        for i in range(0,8):
+            print("-"*32)
+            print(chr(i+97),end="|")
+            for j in range(0,8):
+                item = self.gameboard.get((i,j)," ")
+                print(str(item) + ' |', end=" ")
+            print()
+        print("-"*32)
+
+class Piece:
+    def __init__(self, color, name):
+        self.name = name
+        self.Color = color
+    
+    def isValid(self, startpos, endpos, Color, gameboard):
+        return endpos in self.availableMoves(startpos[0], startpos[1], gameboard, Color)
+    
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    def availableMoves(self, x, y, gameboard, Color):
+        print("ERROR: no movement for base class")
+
+    def AdNauseum(self, x, y, gameboard, Color, intervals):
+        answers = []
+        for xint, yint in intervals:
+            xtemp, ytemp = x + xint, y + yint
+            while self.isInBounds(xtemp, ytemp):
+                target = gameboard.get((xtemp, ytemp), None)
+                if target is None:
+                    answers.append((xtemp, ytemp))
+                elif target.Color != Color:
+                    answers.append((xtemp, ytemp))
+                    break
+                else:
+                    break
+                xtemp, ytemp = xtemp + xint, ytemp + yint
+        return answers
+
+    def isInBounds(self, x, y):
+        return 0 <= x < 8 and 0 <= y < 8
+
+    def noConflict(self, gameboard, initialColor, x, y):
+        return self.isInBounds(x, y) and (((x, y) not in gameboard) or gameboard[(x, y)].Color != initialColor)
+
+chessCardinals = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+chessDiagonals = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
+
+class Rook(Piece):
+    def availableMoves(self, x, y, gameboard, Color = None):
+        if Color is None:
+            Color = self.Color
+        return self.AdNauseum(x, y, gameboard, Color, chessCardinals)
+        
+class Bishop(Piece):
+    def availableMoves(self, x, y, gameboard, Color = None):
+        if Color is None:
+            Color = self.Color
+        return self.AdNauseum(x, y, gameboard, Color, chessDiagonals)
+        
+class Queen(Piece):
+    def availableMoves(self, x, y, gameboard, Color = None):
+        if Color is None:
+            Color = self.Color
+        return self.AdNauseum(x, y, gameboard, Color, chessCardinals + chessDiagonals)
+
+class King(Piece):
+    def availableMoves(self, x, y, gameboard, Color = None):
+        if Color is None:
+            Color = self.Color
+        return [(xx, yy) for xx, yy in kingList(x, y) if self.noConflict(gameboard, Color, xx, yy)]
+
+class Pawn(Piece):
+    def __init__(self, color, name, direction):
+        self.name = name
+        self.Color = color
+        self.direction = direction
+    
+    def availableMoves(self, x, y, gameboard, Color = None):
+        if Color is None:
+            Color = self.Color
+        answers = []
+        if (x+1, y+self.direction) in gameboard and self.noConflict(gameboard, Color, x+1, y+self.direction):
+            answers.append((x+1, y+self.direction))
+        if (x-1, y+self.direction) in gameboard and self.noConflict(gameboard, Color, x-1, y+self.direction):
+            answers.append((x-1, y+self.direction))
+        if (x, y+self.direction) not in gameboard:
+            answers.append((x, y+self.direction))
+        return answers
+
+uniDict = {WHITE: {Pawn: "P", Rook: "R", Bishop: "B", King: "K", Queen: "Q"},
+           BLACK: {Pawn: "P", Rook: "R", Bishop: "B", King: "K", Queen: "Q"}}
+
+Game()
